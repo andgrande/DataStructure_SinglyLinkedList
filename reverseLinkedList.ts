6f37602aaf959e8509bd15a72502b71beeb89f60
Anderson Luiz <andgrande@hotmail.com>
2021-12-01 09:46:43 -0300

    Create reverseLinkedList.ts
    In this algorithm, each node will be moved exactly once.
    
    Therefore, the time complexity is O(N), where N is the length of the linked list. We only use constant extra space so the space complexity is O(1).

---
+++ reverseLinkedList.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let prev = null;
+    let cur = head;
+    let next;
+    
+    while (cur) {
+        next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+    
+    return prev;
+};
+
+// another approach
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let [prev, cur] = [null, head];
+
+  while (cur) {
+    [cur.next, prev, cur] = [prev, cur, cur.next];
+  }
+
+  return prev;
+}
